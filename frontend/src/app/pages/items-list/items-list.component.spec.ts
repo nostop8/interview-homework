c4aa564031410e4fa78457b986234e52da986347
Volodymyr Dovbenko <volodymyr.dovbenko@tomorrow.io>
2025-08-11 00:07:47 +0200

    Implemented loading and fail state for list items, added tests

--- frontend/src/app/pages/items-list/items-list.component.spec.ts
+++ frontend/src/app/pages/items-list/items-list.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed, tick } from '@angular/core/testing';
 
 import { ItemsListComponent } from './items-list.component';
 import { ProductService } from 'src/app/services/product.service';
-import { of } from 'rxjs';
+import { delay, filter, of, throwError } from 'rxjs';
 
 describe('ItemsListComponent', () => {
   let component: ItemsListComponent;
@@ -50,10 +50,49 @@ describe('ItemsListComponent', () => {
 
   it('should fetch products on init', (done) => {
     expect(productServiceMock.getProducts).toHaveBeenCalled();
-    component.items$.subscribe((products) => {
-      expect(products).toEqual(mockProducts);
+    component.state$.pipe(filter(({ items }) => items !== undefined && items.length > 0)).subscribe((data) => {
+      expect(data).toEqual({
+        loading: false,
+        error: false,
+        items: mockProducts,
+      });
+      expect(fixture.nativeElement.querySelector('.message')).toBeFalsy();
       done();
     });
+  });
+
+  it('should show loading state initially', () => {
+    productServiceMock.getProducts.and.returnValue(of(mockProducts).pipe(delay(100)));
+    fixture = TestBed.createComponent(ItemsListComponent);
+    component = fixture.componentInstance;
+
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('.message').textContent).toContain('Loading... Please wait.');
+  });
+
+  it('should show error state if fetching products fails', () => {
+    productServiceMock.getProducts.and.returnValue(throwError(() => new Error('Failed to fetch products')));
+    fixture = TestBed.createComponent(ItemsListComponent);
+    component = fixture.componentInstance;
+
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('.message').textContent).toContain('Failed to load items.');
+  });
+
+  it('should show empty state if no items are available', () => {
+    productServiceMock.getProducts.and.returnValue(of([]));
+    fixture = TestBed.createComponent(ItemsListComponent);
+    component = fixture.componentInstance;
+
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('.message').textContent).toContain('No items found.');
+  });
+
+  it('should set editableItemId when editItem is called', () => {
+    const productIdToEdit = 1;
+    component.editItem(productIdToEdit);
+    expect(component.editableItemId).toBe(productIdToEdit);
   });
 
   it('should call deleteProduct and then getProducts on delete', () => {
@@ -66,7 +105,7 @@ describe('ItemsListComponent', () => {
   });
 
   it('should call getProducts on reload', () => {
-    component.reload();
+    component.getItems();
     fixture.detectChanges();
     expect(productServiceMock.getProducts).toHaveBeenCalledTimes(2);
   });
--- frontend/src/app/pages/items-list/items-list.component.ts
+++ frontend/src/app/pages/items-list/items-list.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListItemComponent } from './list-item/list-item.component';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable, of, startWith } from 'rxjs';
 import { WarehouseItem } from '../../core/models/warehouseItem';
 import { ItemsMockService } from './items.mock.service';
 import { ProductService } from 'src/app/services/product.service';
 import { ItemFormComponent } from './item-form/item-form.component';
+
+interface ItemsState {
+  loading: boolean;
+  error: boolean;
+  items?: WarehouseItem[];
+}
 
 @Component({
   selector: 'app-items-list',
@@ -14,26 +20,46 @@ import { ItemFormComponent } from './item-form/item-form.component';
   templateUrl: './items-list.component.html',
   styleUrls: ['./items-list.component.scss'],
 })
-export class ItemsListComponent {
+export class ItemsListComponent implements OnInit {
   editableItemId: number | null = null;
-
-  items$: Observable<WarehouseItem[]> = this.productService.getProducts();
-
+  state$: Observable<ItemsState>;
   constructor(private itemsMockService: ItemsMockService, private productService: ProductService) {}
 
   addItemToShipment(id: number): void {
     this.itemsMockService.addToShipment(id);
   }
 
-  reload() {
-    this.items$ = this.productService.getProducts();
+  ngOnInit(): void {
+    this.getItems();
+  }
+
+  getItems() {
+    this.state$ = this.productService.getProducts().pipe(
+      map((items) => ({
+        loading: false,
+        error: false,
+        items,
+      })),
+      startWith({
+        loading: true,
+        error: false,
+        items: undefined,
+      }),
+      catchError(() =>
+        of({
+          loading: false,
+          error: true,
+          items: undefined,
+        })
+      )
+    );
     this.editableItemId = null;
   }
 
   deleteItem(productId: number): void {
     confirm('Are you sure you want to delete this product?') &&
       this.productService.deleteProduct(productId).subscribe(() => {
-        this.reload();
+        this.getItems();
       });
   }
 
